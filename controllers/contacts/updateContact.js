5e45cd441d4003ba050bb6c4e19ac5ca120566a9
Leskovets Maksym <leskovecmaksim@gmail.com>
2021-11-30 23:19:36 +0100

    fix: 1. added new validation in updateContact and updateStatusContact; 2. added .env to file .gitignore

--- controllers/contacts/updateContact.js
+++ controllers/contacts/updateContact.js
@@ -1,9 +1,14 @@
 const { NotFound } = require('http-errors')
+const ObjectID = require('mongoose').Types.ObjectId
 
 const { Contact } = require('../../models')
 
 const updateContact = async (req, res, next) => {
   const { contactId } = req.params
+  const validationId = ObjectID.isValid(contactId)
+  if (validationId === false) {
+    throw new NotFound(`Contact with id=${contactId} not found`)
+  }
   const contact = await Contact.findByIdAndUpdate(contactId, req.body, { new: true })
   if (!contact) {
     throw new NotFound(`Contact with id=${contactId} not found`)
--- controllers/contacts/updateStatusContact.js
+++ controllers/contacts/updateStatusContact.js
@@ -1,10 +1,15 @@
 const { NotFound } = require('http-errors')
+const ObjectID = require('mongoose').Types.ObjectId
 
 const { Contact } = require('../../models')
 
 const updateStatusContact = async (req, res, next) => {
   const { contactId } = req.params
   const { favorite } = req.body
+  const validationId = ObjectID.isValid(contactId)
+  if (validationId === false) {
+    throw new NotFound(`Contact with id=${contactId} not found`)
+  }
   const contact = await Contact.findByIdAndUpdate(contactId, { favorite }, { new: true })
   if (!contact) {
     throw new NotFound(`Contact with id=${contactId} not found`)
